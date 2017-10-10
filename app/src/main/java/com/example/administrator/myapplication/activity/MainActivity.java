14645cd94f5fde5454e18ff943b4939918b7aa99
strawberrycy <1635304947@qq.com>
2017-10-10 10:15:08 +0800

    技巧1完成

---
+++ app/src/main/java/com/example/administrator/myapplication/activity/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.administrator.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.hack1:
+                intent = new Intent(MainActivity.this,Hack1Activity.class);
+                startActivity(intent);
+                break;
+        }
+
+    }
+}
